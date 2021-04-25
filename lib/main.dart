baf6ba5875afe664080212ccca3c278c960800d2
Ivan Gonzalez <ivan@iconos.mx>
2021-04-25 14:35:25 -0700

    Creando la ruta de producto page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // mateapp
 import 'package:flutter/material.dart';
 import 'package:flutter_curso_productos/pages/home_page.dart';
+import 'package:flutter_curso_productos/pages/producto_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       initialRoute: 'home',
       routes: {
-        'home' : ( BuildContext context ) => HomePage()
+        'home' : ( BuildContext context ) => HomePage(),
+        'producto' : ( BuildContext context ) => ProductoPage(),
       },
       theme: ThemeData(
         primaryColor: Colors.deepPurple,
--- lib/pages/producto_page.dart
+++ lib/pages/producto_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ProductoPate extends StatelessWidget {
-  const ProductoPate({Key key}) : super(key: key);
+class ProductoPage extends StatelessWidget {
+  const ProductoPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
